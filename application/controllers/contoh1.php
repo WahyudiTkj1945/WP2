797111e1721c0cd326b527fae70df2c945fb88e4
WahyudiSetiawan <wahyudiyuditkj@gmail.com>
2023-09-27 14:48:26 +0700

    menambahkan folder

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class  Contoh1  extends  CI_Controller
+{
+     public function public()
+    {
+        echo " <h1>Perkenalan Diri</h1> ";
+        echo " Nama : Wahyudi Setiawan <br> ";
+        echo " Nim : 12220637 <br> ";
+        echo " Kelas: 12.3A.05 <br> ";
+        echo " TTL : Jakarta, 05 Juli 2002 <br> ";
+    }
+}
